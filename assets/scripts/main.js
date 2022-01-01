551602dce3ebcacb8c57e89c3e890fe176620db5
unknown <nicktest701@gmail.com>
2022-01-01 21:02:55 +0000

    add changes to css file

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,17 @@
 let scrollBtn = document.getElementById("scroll-button");
+let menu = document.querySelector(".menu");
+let overlay = document.querySelector(".overlay");
 
 window.addEventListener("scroll", (e) => {
   window.scrollY > 500
     ? (scrollBtn.style.display = "block")
     : (scrollBtn.style.display = "none");
+});
+
+menu.addEventListener("click", (e) => {
+  overlay.classList.toggle("close");
+});
+
+overlay.addEventListener("click", (e) => {
+  overlay.classList.remove("close");
 });
